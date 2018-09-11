c2d50574f470d0cd53dfbcad75a57d9f2b9122ff
Matt Ryan <matt@capwebsolutions.com>
2018-09-11 19:58:26 -0400

    add core funt pi constant

--- mu-plugins/starter-core-functionality/starter-core-functionality.php
+++ mu-plugins/starter-core-functionality/starter-core-functionality.php
@@ -17,36 +17,44 @@
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  */
 
-// Plugin Directory. Set constant so we know where we are installed
-define( 'CWS_DIR', dirname( __FILE__ ) );
+  // Exit if accessed directly.
+  defined( 'ABSPATH' ) || exit;
+
+  // Plugin Directory. Set constant so we know where we are installed.
+  $plugin_url = plugin_dir_url( __FILE__ );
+  if ( is_ssl() ) {
+	  $plugin_url = str_replace( 'http://', 'https://', $plugin_url );
+  }
+  define( 'CORE_FUNCTION_URL', $plugin_url );
+  define( 'CORE_FUNCTION_DIR', plugin_dir_path( __FILE__ ) );
 
 // General. This should always be used. 
-include_once( CWS_DIR . '/lib/functions/general.php' );
+include_once( CORE_FUNCTION_DIR . '/lib/functions/general.php' );
 
 // Define needed Custom Post Types.
-// include_once( CWS_DIR . '/lib/functions/post-types.php' );
+// include_once( CORE_FUNCTION_DIR . '/lib/functions/post-types.php' );
 
 // Define needed Custom Taxonomies.
-// include_once( CWS_DIR . '/lib/functions/taxonomies.php' );
+// include_once( CORE_FUNCTION_DIR . '/lib/functions/taxonomies.php' );
 
 // Define Custom Meta boxes.
-// include_once( CWS_DIR . '/lib/functions/metaboxes.php' );
+// include_once( CORE_FUNCTION_DIR . '/lib/functions/metaboxes.php' );
 
 // Footer Setup.This should always be used. 
-include_once( CWS_DIR . '/lib/functions/core-footer.php' );
+include_once( CORE_FUNCTION_DIR . '/lib/functions/core-footer.php' );
 
 // Woo tweaks. Only if WooCommerce active.
 if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
 // or if ( class_exists( 'WooCommerce' ) ) {
-	include_once( CWS_DIR . '/lib/functions/wootweaks.php' );
+	include_once( CORE_FUNCTION_DIR . '/lib/functions/wootweaks.php' );
 }
 
 
 // Gravity Forms tweaks. This should always be used if Gravity Forms active. Which one to use??
 if ( in_array( 'gravityforms/gravityforms.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
-	include_once( CWS_DIR . '/lib/functions/gravitytweaks.php' );
+	include_once( CORE_FUNCTION_DIR . '/lib/functions/gravitytweaks.php' );
 }
 // or 
 if ( class_exists( 'GFForms' ) ) { 
-	include_once( CWS_DIR . '/lib/functions/gravitytweaks.php' );
+	include_once( CORE_FUNCTION_DIR . '/lib/functions/gravitytweaks.php' );
 }
